845e594014c85caa5be8609389b0b74f113703a4
Maksym-Gorbunov <maxsverige@hotmail.com>
2019-11-02 21:40:36 +0100

    rest

--- src/main/java/com/springBootJPA/controller/DevController.java
+++ src/main/java/com/springBootJPA/controller/DevController.java
@@ -5,6 +5,7 @@ import com.springBootJPA.model.dao.DeveloperRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -59,5 +60,16 @@ public class DevController {
     mv.addObject("result", result);
     return mv;
   }
+
+
+  ////////////////////////////// REST ///////////////////////////////////////
+
+  @RequestMapping("developers")
+  @ResponseBody
+  public String getDevelopers(){
+    return developerRepo.findAll().toString();
+  }
+
+
 
 }
