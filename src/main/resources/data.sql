d54f0ac9fb026a6466b21432e32dda0eb7d1237f
Maksym-Gorbunov <maxsverige@hotmail.com>
2019-11-02 14:55:53 +0100

    data.sql updated

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,5 @@
 insert into developer values(1, 'Maks', 'Java');
 insert into developer values(2, 'Peter', 'Php');
 insert into developer values(3, 'Tom', 'JavaScript');
+insert into developer values(4, 'Kim', 'Python');
+insert into developer values(5, 'Leif', 'Ruby');
